cdc8c397875ffb9dfe505369ec8e519ec09a9866
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-11-14 13:53:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(num) {
+    index = (num + slides.length) % slides.length;
+    slides.forEach(s => s.classList.remove("active"));
+    slides[index].classList.add("active");
+}
+
+setInterval(() => showSlide(index + 1), 4000);
+
+document.getElementById("prev").onclick = () => showSlide(index - 1);
+document.getElementById("next").onclick = () => showSlide(index + 1);
