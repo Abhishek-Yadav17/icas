1e074ea9b64e7fa5aadaeded6d91ee1f081c1100
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-11-20 19:12:51 +0530

    added courses page

--- script.js
+++ script.js
@@ -9,9 +9,6 @@ function showSlide(num) {
 
 setInterval(() => showSlide(index + 1), 4000);
 
-document.getElementById("prev").onclick = () => showSlide(index - 1);
-document.getElementById("next").onclick = () => showSlide(index + 1);
-
 const menuIcon = document.querySelector(".menu-icon");
 const mobileMenu = document.querySelector(".mobile-menu");
 
