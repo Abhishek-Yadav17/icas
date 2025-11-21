5a35e1245968c4bf0cefa369f9c76840ba2fa5c6
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-11-21 20:14:33 +0530

    added video

--- courses.js
+++ courses.js
@@ -4,3 +4,23 @@ const mobileMenu = document.querySelector(".mobile-menu");
 menuIcon.addEventListener("click", () => {
     mobileMenu.classList.toggle("show");
 });
+
+document.querySelectorAll(".video-box").forEach(box => {
+    const video = box.querySelector("video");
+    const btn = box.querySelector(".play-btn");
+
+    btn.onclick = () => {
+        if (video.paused) {
+            video.play();
+            btn.style.display = "none";
+        } else {
+            video.pause();
+            btn.style.display = "block";
+        }
+    };
+
+    video.onclick = () => {
+        video.pause();
+        btn.style.display = "block";
+    };
+});
